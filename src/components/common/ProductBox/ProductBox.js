2b6c764922aa041442af5163c922e47392af1370
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2019-12-04 19:56:55 +0100

    Add hover effects on ProductBox buttons and price

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -16,8 +16,10 @@ const ProductBox = ({ name, price, promo, stars }) => (
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
       <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
+        <Button className={styles.productBoxBtn} variant='small'>
+          Quick View
+        </Button>
+        <Button className={styles.productBoxBtn} variant='small'>
           <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
         </Button>
       </div>
@@ -47,7 +49,7 @@ const ProductBox = ({ name, price, promo, stars }) => (
         </Button>
       </div>
       <div className={styles.price}>
-        <Button noHover variant='small'>
+        <Button className={styles.price} noHover variant='small'>
           $ {price}
         </Button>
       </div>
