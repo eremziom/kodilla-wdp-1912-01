9cdd42452278a86926c1998536ec37f40fca47ad
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2019-12-04 22:09:14 +0100

    Add products old price display posibility

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,7 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars }) => (
+const ProductBox = ({ name, price, oldPrice, promo, stars }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -46,6 +46,11 @@ const ProductBox = ({ name, price, promo, stars }) => (
           <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
         </Button>
       </div>
+      {oldPrice ? (
+        <div className={styles.price}>
+          <span className={styles.oldPrice}>$ {oldPrice}</span>
+        </div>
+      ) : null}
       <div className={styles.price}>
         <Button noHover variant='small'>
           $ {price}
@@ -59,6 +64,7 @@ ProductBox.propTypes = {
   children: PropTypes.node,
   name: PropTypes.string,
   price: PropTypes.number,
+  oldPrice: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
 };
