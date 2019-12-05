511fc09c8498b477e4d089f9a18c2ab003e79dcc
Angelika Draczynska <nayrissa@outlook.com>
2019-12-05 16:18:09 +0100

    Add state for favorites products

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,7 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars }) => (
+const ProductBox = ({ name, price, promo, stars, addToFavorites, id, favorites }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -38,14 +38,19 @@ const ProductBox = ({ name, price, promo, stars }) => (
     </div>
     <div className={styles.line}></div>
     <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button variant='outline'>
+      <div className={styles.outline}>
+        <Button
+          className={favorites === true ? styles.favorites : styles.outlines}
+          onClick={() => addToFavorites({ id })}
+          variant='outline'
+        >
           <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
         </Button>
         <Button variant='outline'>
           <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
         </Button>
       </div>
+
       <div className={styles.price}>
         <Button noHover variant='small'>
           $ {price}
@@ -61,6 +66,9 @@ ProductBox.propTypes = {
   price: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  favorites: PropTypes.bool,
+  addToFavorites: PropTypes.func,
+  id: PropTypes.string,
 };
 
 export default ProductBox;
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,28 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+/* action name creator */
+
+const reducerName = 'products';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+export const ADD_FAVORITES = createActionName('ADD_FAVORITES');
+
+/* action creators */
+export const createAction_addFavorites = payload => ({ payload, type: ADD_FAVORITES });
+
 /* reducer */
+
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case ADD_FAVORITES:
+      return statePart.map(product => {
+        if (product.id === action.payload.id) {
+          product.favorites = true;
+        }
+        return product;
+      });
     default:
       return statePart;
   }
