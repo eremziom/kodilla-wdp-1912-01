b7b2129574694c2119ecbc194cebce1c32f28592
Angelika Draczynska <nayrissa@outlook.com>
2019-12-05 17:26:57 +0100

    Rename action type

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -11,16 +11,19 @@ const reducerName = 'products';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 /* action types */
-export const ADD_FAVORITES = createActionName('ADD_FAVORITES');
+export const ADD_TO_FAVORITES = createActionName('ADD_TO_FAVORITES');
 
 /* action creators */
-export const createAction_addFavorites = payload => ({ payload, type: ADD_FAVORITES });
+export const createAction_addFavorites = payload => ({
+  payload,
+  type: ADD_TO_FAVORITES,
+});
 
 /* reducer */
 
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
-    case ADD_FAVORITES:
+    case ADD_TO_FAVORITES:
       return statePart.map(product => {
         if (product.id === action.payload.id) {
           product.favorites = true;
